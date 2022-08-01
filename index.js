7f9fb62dfa89c267aac0aa86270d08e5b7e2fb02
MoniqueDampies <moniquedampies1861@gmail.com>
2022-08-01 11:36:12 +0200

    Changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = parseInt(process.env.port) || 4000;
+const port = parseInt(process.env.PORT) || 4000;
 let courses = [
     {
         id: 1, name: 'Software Engineering'
